476f87c51fd85a0805b3336b4366834b590e2824
laingyilee <milly0105@gmail.com>
2017-08-17 21:56:16 +0800

    Update 2017-08-17T13:56:14.607Z

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@
 var area = document.getElementById('s-area');
 //撈取資料
 var data = [];
-var url = "https://github.com/laingyilee/travelinfo/blob/master/data.json";
+var url = "https://laingyilee.github.io/travelinfo/js/data.json";
 var xhr = new XMLHttpRequest();
 var content;
 var len;
